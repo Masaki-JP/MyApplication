d940e3d7f12c7ad40a5b51fc026b845ac013d164
Masaki-JP <masakidayo0508@gmail.com>
2024-09-20 01:10:16 +0900

    Initial Commit

---
+++ MyApplication/MyApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApplicationApp.swift
+//  MyApplication
+//
+//  Created by Masaki Doi on 2024/09/20.
+//
+
+import SwiftUI
+
+@main
+struct MyApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
