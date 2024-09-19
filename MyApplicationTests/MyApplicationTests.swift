6b665628c5e4c6aa51a0dd3493c6cdefcb63c38d
Masaki-JP <masakidayo0508@gmail.com>
2024-09-20 02:38:41 +0900

    コメントアウト

--- MyApplicationTests/MyApplicationTests.swift
+++ MyApplicationTests/MyApplicationTests.swift
@@ -28,11 +28,11 @@ final class MyApplicationTests: XCTestCase {
         XCTAssertEqual(1 + 1, 2)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
